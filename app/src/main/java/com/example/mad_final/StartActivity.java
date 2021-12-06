bbbb022b32d6105441b7ba875ef00035886c8525
youngkwon02 <youngkwon02@naver.com>
2021-12-06 17:56:33 +0900

    feat: DB connection and update 5 questions

--- app/src/main/java/com/example/mad_final/StartActivity.java
+++ app/src/main/java/com/example/mad_final/StartActivity.java
@@ -3,8 +3,8 @@ package com.example.mad_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -12,13 +12,19 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.mad_final.databinding.ActivityMainBinding;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Calendar;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
 
 public class StartActivity extends AppCompatActivity {
 
-    private TextView wdQuiz, wdQuizNo;
+    private TextView wdQuiz, wdQuizNo, wdResult, reDate, reScore;
     private RadioGroup radioGroup;
     private RadioButton rb1, rb2, rb3, rb4, rb5;
     private Button btnNext;
@@ -26,12 +32,78 @@ public class StartActivity extends AppCompatActivity {
     int totalQuiz;
     int quizCounter = 0;
     int score;
+    int year;
+    int month;
+    int week;
 
     ColorStateList dfRbColor;
 
     private QuizModel currentQuiz;
 
     private List<QuizModel> questionsList;
+
+    private ActivityMainBinding binding;
+
+    public class myDBHelper extends SQLiteOpenHelper{
+        public myDBHelper(StartActivity context){
+            super(context, "wdlDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("create table wdlTBL (year char(10), month char(10), week char(10), " +
+                    "score integer);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+            db.execSQL("drop table if exists wdlTBL;");
+            onCreate(db);
+        }
+
+    }
+
+    public void initializeDB(View view){
+        myDBHelper myHelper = new myDBHelper(this);
+        SQLiteDatabase sqlDB = myHelper.getWritableDatabase();
+        myHelper.onUpgrade(sqlDB,1,2);
+        sqlDB.close();
+        Toast.makeText(getApplicationContext(),"Initialized",Toast.LENGTH_LONG).show();
+    }
+
+    public void insertDB(){
+        myDBHelper myHelper = new myDBHelper(this);
+        SQLiteDatabase sqlDB = myHelper.getWritableDatabase();
+        sqlDB.execSQL("delete from wdlTBL where year = " + year + " and month = " + month + " and week =" + week);
+        sqlDB.execSQL("insert into wdlTBL values (" + year + ", " + month + "," + week+
+                "," + score +")");
+        sqlDB.close();
+    }
+
+    public void searchDB(){
+        myDBHelper myHelper = new myDBHelper(this);
+        SQLiteDatabase sqlDB = myHelper.getReadableDatabase();
+        Cursor cursor;
+        cursor = sqlDB.rawQuery("select * from wdlTBL;",null);
+
+        String string1 = "날짜\n\n";
+        String string2 = "점수\n\n";
+
+        while(cursor.moveToNext()){
+            String re_year = cursor.getString(0);
+            String re_month = cursor.getString(1);
+            String re_week = cursor.getString(2);
+            String re_score = cursor.getString(3);
+            string1 += re_year + "년 " + re_month + "월 " + re_week +"주차\n";
+            string2 += re_score + "점\n";
+        }
+
+        reDate.setText(string1);
+        reScore.setText(string2);
+
+        cursor.close();
+        sqlDB.close();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +113,9 @@ public class StartActivity extends AppCompatActivity {
         questionsList = new ArrayList<>();
         wdQuiz = findViewById(R.id.wdQuiz);
         wdQuizNo = findViewById(R.id.wdQuizNo);
+        wdResult = findViewById(R.id.resultText);
+        reDate = findViewById(R.id.resultDate);
+        reScore = findViewById(R.id.resultScore);
 
         radioGroup = findViewById(R.id.radioGroup);
         rb1 = findViewById(R.id.rb1);
@@ -63,15 +138,15 @@ public class StartActivity extends AppCompatActivity {
                 if(rb1.isChecked() || rb2.isChecked() || rb3.isChecked() || rb4.isChecked()
                         || rb5.isChecked()) {
                     if(rb1.isChecked()) {
-                        score += 10;
+                        score += 4;
                     } else if(rb2.isChecked()) {
-                        score += 20;
+                        score += 8;
                     } else if(rb3.isChecked()) {
-                        score += 30;
+                        score += 12;
                     } else if(rb4.isChecked()) {
-                        score += 40;
+                        score += 16;
                     } else if(rb5.isChecked()) {
-                        score += 50;
+                        score += 20;
                     }
                     showNextQuiz();
                 } else {
@@ -106,14 +181,28 @@ public class StartActivity extends AppCompatActivity {
 
         } else if(quizCounter == totalQuiz){
             Calendar cal = Calendar.getInstance();
-            int year = cal.get(Calendar.YEAR);
-            int month = cal.get(Calendar.MONTH) + 1;
+            year = cal.get(Calendar.YEAR);
+            month = cal.get(Calendar.MONTH) + 1;
             int date = cal.get(Calendar.DATE);
             int day_of_week = cal.get(Calendar.DAY_OF_WEEK);
-            String dowArr[] = {"일", "월", "화", "수", "목", "금", ""};
+            int offset = 7 - day_of_week;
+            int satDate = date + offset;
+            week = (satDate / 7) + 1;
+            String dowArr[] = {"일", "월", "화", "수", "목", "금", "토"};
             String dow = dowArr[day_of_week - 1];
             wdQuizNo.setText("");
-            wdQuiz.setText(year + "년 " + month + " 월" + date + " 일 (" + dow + ")\nScore: " + score);
+            wdQuiz.setText("");
+            wdResult.setText("\n" + month + " 월 " + week + "주차 응답 완료!" + "\n" +
+                    "이번주 슬기로움: " + score + "점");
+            insertDB();
+            searchDB();
+            btnNext.setText("Finish");
+            btnNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
             radioGroup.setVisibility(View.INVISIBLE);
         } else {
             finish();
@@ -121,20 +210,16 @@ public class StartActivity extends AppCompatActivity {
     }
 
     private void addQuestions() {
-        questionsList.add(new QuizModel("Pineapple pizza, are you serious?", "Yes",
-                "No", "wtf", "Not worth mentioning", "Mint pizza would" +
-                " be better"));
-        questionsList.add(new QuizModel("Mint pizza, are you serious?", "Yes",
-                "No", "wtf", "Not worth mentioning", "Mint pizza would" +
-                " be better"));
-        questionsList.add(new QuizModel("Chocolate pizza, are you serious?", "Yes",
-                "No", "wtf", "Not worth mentioning", "Mint pizza would" +
-                " be better"));
-        questionsList.add(new QuizModel("Sugar pizza, are you serious?", "Yes",
-                "No", "wtf", "Not worth mentioning", "Mint pizza would" +
-                " be better"));
-        questionsList.add(new QuizModel("Spicy pizza, are you serious?", "Yes",
-                "No", "wtf", "Not worth mentioning", "Mint pizza would" +
-                " be better"));
+        questionsList.add(new QuizModel("이번주에 계획한 시간에 일어난 일수", "0 ~ 1일",
+                "2 ~ 3일", "4일", "5 ~ 6일", "7일 모두"));
+        questionsList.add(new QuizModel("이번주에 친구와의 약속에 늦지 않은 횟수", "1회 이하",
+                "2회", "3회", "4회", "5회 이상"));
+        questionsList.add(new QuizModel("이번주에 내가 좋아하는 일을 한 횟수", "1회 이하",
+                "2회", "3회", "4회", "5회 이상"));
+        questionsList.add(new QuizModel("이번주에 누군가를 웃으면서 대한 횟수", "2회 이하",
+                "3회", "4회", "5회", "6회 이상"));
+        questionsList.add(new QuizModel("이번주에 누군가에게 받은 칭찬 횟수", "없음",
+                "1회", "2회", "3회", "4회 이상"));
     }
+
 }
